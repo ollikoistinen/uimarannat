9766782865372fbb8491e8ec2b2b027b9f9b4ef7
Olli Koistinen <olli@voxelpop.games>
2024-06-26 18:08:23 +0300

    Initial commit - a new lein default project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to uimarannat
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/uimarannat/core_test.clj
@@ -0,0 +1,7 @@
+(ns uimarannat.core-test
+  (:require [clojure.test :refer :all]
+            [uimarannat.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
